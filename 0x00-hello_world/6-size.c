74dddf58d7861714d0fdf3138b0c6ed757223ef4
KING <francisking1st@gmail.com>
2023-06-15 17:37:32 -0700

    second commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,10 @@
-#include <studio.h>
+#include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", (int) sizeof(char));
